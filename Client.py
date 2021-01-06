33f698afaf3d521b54aa58a62f44807d7884d9e1
fmolliet <fmolliet@hotmail.com>
2021-01-06 12:16:13 -0300

    Finalizado montagem de variaveis randomicamentes

--- Client.py
+++ Client.py
@@ -1,6 +1,7 @@
 from telethon import TelegramClient, events, sync
 from decouple import config
 import json
+from random import choice
 
 class Automessager(object): 
 
@@ -17,4 +18,50 @@ class Automessager(object):
         with open(self.filename) as json_file:
             data = json.load(json_file)
             for friend in data['friends']:
-                self.client.send_message(friend['id'], 'Testando...')
+                self.aliases = self.get_aliases_choices(friend['alias'], friend['name'])
+                
+                self.client.send_message(friend['id'], self.get_rand_msg_dia(friend['has_affetuos']))
+                
+                if friend['has_work'] :
+                    self.client.send_message(friend['id'], self.get_rand_msg_trabalho())
+                    
+    def get_rand_msg_dia(self, has_affetuos) -> str:
+        possible_messages = [
+            'Bom dia',
+            'Bundia',
+            'Bom dia hoje pra ti'
+        ]
+        
+        if has_affetuos :
+            possible_messages.append([
+                "bom dia meu lindo",
+                "Bom dia fofo"
+            ])
+            
+        return "{} {}".format( choice(possible_messages), choice(self.aliases)  )
+        
+    def get_rand_msg_trabalho(self) -> str:
+        possible_messages = [
+            'Bom trabalho pra ti',
+            'Tenha um bom trabalho',
+            'Bom trabson',
+            'Bom trabalho hoje',
+        ]
+
+        return choice(possible_messages)
+    
+    
+    def get_aliases_choices(self, alias, name) -> object:
+        if alias :
+            return [ 
+                ""
+                "{}".format(choice(alias)),
+                "{}!".format(choice(alias))
+            ]
+        else: 
+            return [ 
+                ""
+                "{}".format(name),
+                "{}!".format(name)
+            ]
+    
